7557e63cc8fcffcdd769e69e85443572b8d4ba34
AnkitParte <ankitparte80@gmail.com>
2022-09-19 12:03:44 +0530

    all set

--- resources/scripts/index.js
+++ resources/scripts/index.js
@@ -9,7 +9,14 @@ copy.addEventListener("click", () => {
     }, 1000);
 });
 
-
+function myFunction() {
+    var x = document.getElementById("myLinks");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
+}
 
 
 
